02505b593dd3f30a807c6c8987ea54afc1479f55
Luke Godfrey <thelukester92@me.com>
2018-02-20 13:28:27 -0600

    Done loading model and images.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from keras.preprocessing.image import load_img
+from keras.preprocessing import image
 from model import make_model
 
 p = ArgumentParser()
@@ -7,7 +7,11 @@ p.add_argument('content_image', type=str)
 p.add_argument('style_image', type=str)
 args = p.parse_args()
 
-content_image = load_img(args.content_image)
-style_image = load_img(args.style_image)
+content_image = image.img_to_array(image.load_img(args.content_image))
+style_image = image.img_to_array(image.load_img(args.style_image,
+    target_size=content_image.shape))
 
-model = make_model(1024, 1024)
+width = content_image.shape[1]
+height = content_image.shape[0]
+
+model = make_model(width, height)
