aec9172a36971f083c39cdc4d83601107a74b80b
Luke Godfrey <thelukester92@me.com>
2018-03-13 14:16:04 -0500

    Done with content loss optimized.

--- style_net.py
+++ style_net.py
@@ -15,26 +15,29 @@ class StyleNet():
                  style_layers=['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']):
         self.content_layer = content_layer
         self.style_layers = style_layers
+        self.model = load_vgg16((None, None, 3), include_top=False)
 
     def apply_style(self, content_image, style_image, display=False, iterations=1000, **kwargs):
-        model = load_vgg16((None, None, 3), include_top=False)
-
-        content_outputs = [x.output for x in model.layers if x.name == self.content_layer]
-        content_func = K.function(model.inputs, content_outputs)
+        content_outputs = [x.output for x in self.model.layers if x.name == self.content_layer]
+        content_func = K.function(self.model.inputs, content_outputs)
         content_features = content_func([np.expand_dims(content_image, axis=0)])
 
-        style_outputs = [x.output for x in model.layers if x.name in self.style_layers]
-        style_func = K.function(model.inputs, style_outputs)
+        style_outputs = [x.output for x in self.model.layers if x.name in self.style_layers]
+        style_func = K.function(self.model.inputs, style_outputs)
         style_features = style_func([np.expand_dims(style_image, axis=0)])
 
         generated_image = K.variable(np.expand_dims(content_image, axis=0))
+        generated_content_outputs = Model(inputs=self.model.input, outputs=content_outputs)(generated_image)
+        generated_style_outputs = Model(inputs=self.model.input, outputs=style_outputs)(generated_image)
 
-        c_loss, t_loss, s_loss = self._neural_style_loss(model, content_features, style_features, generated_image, **kwargs)
-        loss = c_loss # c_loss + t_loss + s_loss
+        content_loss = K.mean(K.square(content_features[0] - generated_content_outputs[0]))
+        style_loss = K.constant(0.)
+        variation_loss = K.constant(0.)
+        total_loss = content_loss + style_loss + variation_loss
 
         optimizer = keras.optimizers.adam(lr=1.)
-        updates = optimizer.get_updates(loss=loss, params=model.inputs)
-        f = K.function(model.inputs, [c_loss, t_loss, s_loss], updates=updates)
+        updates = optimizer.get_updates(loss=total_loss, params=[generated_image])
+        f = K.function(self.model.inputs, [content_loss, style_loss, variation_loss], updates=updates)
 
         for i in range(iterations):
             print('{} / {}'.format(i+1, iterations), end='')
@@ -45,20 +48,20 @@ class StyleNet():
                 display_image(gen[0])
 
             losses = f([gen])
-            print(', s_loss={}, c_loss={}, t_loss={}'.format(losses[2], losses[0], losses[1]))
+            print(', content_loss={}, style_loss={}, variation_loss={}'.format(losses[0], losses[1], losses[2]))
             sys.stdout.flush()
         print()
 
         return K.get_value(generated_image)
 
-    def _neural_style_loss(self, model, content_features, style_features, generated_image, content_weight=5., tv_weight=1e2, style_weight=5e2):
-        generated_features = model.get_layer(self.content_layer).output
+    def _neural_style_loss(self, content_features, style_features, generated_outputs, generated_image, content_weight=5., tv_weight=1e2, style_weight=5e2):
+        generated_features = self.model.get_layer(self.content_layer).output
         content_loss = K.mean(K.square(content_features[0] - generated_features[0]))
 
         tv_loss = self._total_variation_loss(generated_image)
 
         style_loss = 0.
-        generated_features = [x.output for x in model.layers if x.name in self.style_layers]
+        generated_features = [x.output for x in self.model.layers if x.name in self.style_layers]
         for style_feat, generated_feat in zip(style_features, generated_features):
             style_loss = style_loss + self._style_loss(style_feat[0], generated_feat[0])
 
