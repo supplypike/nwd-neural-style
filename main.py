6895dfc221188480188aca605c58b75f43b4db3c
Luke Godfrey <thelukester92@me.com>
2018-03-13 14:46:00 -0500

    Added output image path.

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ p = ArgumentParser()
 p.add_argument('content_image', type=str)
 p.add_argument('style_image', type=str)
 p.add_argument('-c', '--cpu_cores', type=int, default=1, help='The number of cores to use when running on a CPU.')
+p.add_argument('-d', '--display', action='store_true')
 p.add_argument('-i', '--iterations', type=int, default=1000, help='The number of iterations to run.')
+p.add_argument('-o', '--output_image', type=str, default=None, help='(Optional) Where to save the styled image.')
 p.add_argument('-s', '--size', type=int, default=None, help='The maximum length of dimension, for resizing the input images.')
-p.add_argument('--no_display', dest='display', action='store_false')
+p.add_argument('--content_weight', type=float, default=5., help='The weight on the content loss.')
+p.add_argument('--style_weight', type=float, default=500., help='The weight on the style loss.')
+p.add_argument('--variation_weight', type=float, default=100., help='The weight on the variation loss.')
 args = p.parse_args()
 
 if args.cpu_cores > 0:
@@ -26,13 +30,24 @@ if args.cpu_cores > 0:
 if args.display:
     pylab.ion()
 
+if not args.display and not args.output_image:
+    print('WARNING: You have not turned on display or provided an output image path, so you will NOT see the styled results.')
+
 content_image = load_image(args.content_image, max_size=args.size)
 style_image = load_image(args.style_image, max_size=args.size)
 
 style_net = StyleNet()
-styled_image = style_net.apply_style(content_image, style_image, display=args.display, iterations=args.iterations)
-display_image(styled_image)
+styled_image = style_net.apply_style(content_image, style_image,
+                                     display=args.display,
+                                     iterations=args.iterations,
+                                     content_weight=args.content_weight,
+                                     style_weight=args.style_weight,
+                                     variation_weight=args.variation_weight)
+
+if args.output_image:
+    save_image(styled_image, args.output_image)
 
 if args.display:
+    display_image(styled_image)
     pylab.ioff()
     pylab.show()
